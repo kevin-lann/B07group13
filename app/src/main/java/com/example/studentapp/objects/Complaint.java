164ae447b77fdeaef40b46bedb09cccbb357cb63
Armaan204 <armaanrehmanshah1@gmail.com>
2023-11-26 23:14:16 -0500

    updated complaint.java
    created complaint object

--- app/src/main/java/com/example/studentapp/objects/Complaint.java
+++ app/src/main/java/com/example/studentapp/objects/Complaint.java
@@ -1,4 +1,35 @@
 package com.example.studentapp.objects;
 
 public class Complaint {
+
+    public int complaintID;
+    public StudentUser student;
+    public String complaintTitle;
+    public String complaintDetails;
+
+    public Complaint(int complaintID, StudentUser student){
+        this.complaintID = complaintID;
+        this.student = student;
+    }
+
+    public Complaint(String complaintTitle, String complaintDetails){
+        this.complaintTitle = complaintTitle;
+        this.complaintDetails = complaintDetails;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if(other == null || !(other instanceof Complaint)) {
+            return false;
+        }
+        Complaint c = (Complaint)other;
+        return c.complaintID == this.complaintID;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 31, d = 67;
+        result += d * complaintID;
+        return result;
+    }
 }
