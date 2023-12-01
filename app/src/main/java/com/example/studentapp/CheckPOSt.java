bd77b33f1c76ca30486c34076a1690594935264a
matthew-p4 <matthew.pazhaidam@mail.utoronto.ca>
2023-12-01 00:37:09 -0500

    finalized check post user story

--- app/src/main/java/com/example/studentapp/CheckPOSt.java
+++ app/src/main/java/com/example/studentapp/CheckPOSt.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,9 +13,18 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.studentapp.databinding.FragmentSecondBinding;
 
-public class SecondFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.Inflater;
 
+public class CheckPOSt extends Fragment {
+
+    public CheckPOSt(){
+        super(R.layout.check_post);
+    }
     private FragmentSecondBinding binding;
+    Spinner spinner;
+    List<String> items;
 
     @Override
     public View onCreateView(
@@ -26,16 +37,28 @@ public class SecondFragment extends Fragment {
 
     }
 
+
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
+                NavHostFragment.findNavController(CheckPOSt.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
+
+        spinner = spinner.findViewById(R.id.spinner);
+
+        items = new ArrayList<>();
+
+        items.add("Statistics");
+        items.add("Mathematics");
+        items.add("Computer Science Minor");
+        items.add("Computer Science Major/Specialist");
+
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,items));
     }
 
     @Override
